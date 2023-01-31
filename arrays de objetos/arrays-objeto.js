892e9bd76723b631ef0a6cb909c568168412324c
Mario Arthur <marioarthursm@outlook.com>
2023-01-31 17:29:49 -0300

    ✅

---
+++ arrays de objetos/arrays-objeto.js
@@ -0,0 +1,36 @@
+let person1 = {
+    name: 'John' ,
+    age: 30,
+
+}
+let person2 = {
+    name: 'Mary' ,
+    age: 20,
+
+}
+let person3 = {
+    name: 'Peter' ,
+    age: 40,
+
+}
+let person4 = {
+    name: 'Jane' ,
+    age: 50,
+
+}
+let person5 = {
+    name: 'Mark' ,
+    age: 60,
+
+}
+
+let list =[person1, person2, person3, person4, person5]
+
+for( let person of list){ //percorre o array de objetos
+ // console.log( person)//exibe o objeto
+ console.log(person.name) //exibe o nome do objeto
+}
+
+//console.log( list) //exibe o array de objetos
+//console.log( list[1]) //exibe o objeto na posição 1
+
